501db200fccb3564b55645571da9fb08de12de5e
Alexander Hammonds <ahammonds247@gmail.com>
2022-03-29 12:49:42 -0500

    adding provider section to each env folder

--- deploy/prod/main.tf
+++ deploy/prod/main.tf
@@ -1,3 +1,15 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" { }
+
 module "deploy-prod" {
   source = "../modules"
   DEPLOY_ENV = "prod"
