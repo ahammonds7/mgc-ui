5c232ff67a29dbc6343262ce66a8ea897a908af0
Alexander Hammonds <ahammonds247@gmail.com>
2022-04-27 19:40:48 -0500

    target health back to false

--- deploy/modules/route-53.tf
+++ deploy/modules/route-53.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "mgc-ui_record_A" {
   type = "A"
 
   alias {
-    evaluate_target_health = true
+    evaluate_target_health = false
     name                   = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.mgc-ui_distribution.hosted_zone_id
   }
@@ -19,7 +19,7 @@ resource "aws_route53_record" "mgc-ui_record_AAAA" {
   type = "AAAA"
 
   alias {
-    evaluate_target_health = true
+    evaluate_target_health = false
     name                   = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
     zone_id                = aws_cloudfront_distribution.mgc-ui_distribution.hosted_zone_id
   }
