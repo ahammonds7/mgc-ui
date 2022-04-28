03a020287cd426914b0a544b394e7f5e70440f0d
Alexander Hammonds <ahammonds247@gmail.com>
2022-04-28 10:21:48 -0500

    basic site out there

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import './App.css';
+import MenuBar from './MenuBar'
+import Content from "./Content";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Magnolia Grove Creations...yay
-      </header>
+    <div>
+      <MenuBar/>
+      <Content/>
     </div>
   );
 }
