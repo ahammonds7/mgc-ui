19bda960abdf4cc80ecbd1c12bb6eaa4e0d2d521
Alexander Hammonds <ahammonds247@gmail.com>
2022-04-27 19:13:15 -0500

    removing route-53 for a sec

--- deploy/modules/route-53.tf
+++ deploy/modules/route-53.tf
@@ -1,34 +1,26 @@
-resource "aws_route53_zone" "mgc-ui_zone" {
-  name = local.domain_name
-}
-
-resource "aws_route53_record" "mgc-ui_record_A" {
-  zone_id = aws_route53_zone.mgc-ui_zone.zone_id
-  name    = local.domain_name
-  type = "A"
-
-  alias {
-    evaluate_target_health = false
-    name                   = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.mgc-ui_distribution.hosted_zone_id
-  }
-}
-resource "aws_route53_record" "mgc-ui_record_AAAA" {
-  zone_id = aws_route53_zone.mgc-ui_zone.zone_id
-  name    = local.domain_name
-  type = "AAAA"
-
-  alias {
-    evaluate_target_health = false
-    name                   = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.mgc-ui_distribution.hosted_zone_id
-  }
-}
-
-output "s3_website_endpoint" {
-  value = aws_s3_bucket.mgc-ui_bucket.website_endpoint
-}
-
-output "cdn_domain" {
-  value = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
-}
+#resource "aws_route53_zone" "mgc-ui_zone" {
+#  name = local.domain_name
+#}
+#
+#resource "aws_route53_record" "mgc-ui_record_A" {
+#  zone_id = aws_route53_zone.mgc-ui_zone.zone_id
+#  name    = local.domain_name
+#  type = "A"
+#
+#  alias {
+#    evaluate_target_health = false
+#    name                   = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
+#    zone_id                = aws_cloudfront_distribution.mgc-ui_distribution.hosted_zone_id
+#  }
+#}
+#resource "aws_route53_record" "mgc-ui_record_AAAA" {
+#  zone_id = aws_route53_zone.mgc-ui_zone.zone_id
+#  name    = local.domain_name
+#  type = "AAAA"
+#
+#  alias {
+#    evaluate_target_health = false
+#    name                   = aws_cloudfront_distribution.mgc-ui_distribution.domain_name
+#    zone_id                = aws_cloudfront_distribution.mgc-ui_distribution.hosted_zone_id
+#  }
+#}
