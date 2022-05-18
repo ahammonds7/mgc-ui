886ab5776d3a6695a7d20411ef4daca4a75553a2
Alexander Hammonds <ahammonds247@gmail.com>
2022-05-18 09:58:29 -0500

    Revert "adding calc page"
    This reverts commit 217c98ecdcbea5613062e8e426aa5a3266d95123.

--- src/Content/index.js
+++ src/Content/index.js
@@ -1,8 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home, { route as homeRoute } from "../Home";
 import Products, { route as productsRoute } from "../Products";
-import AboutUs, { route as aboutUsRoute } from "../AboutUs";
-import Calculations, { route as calculationsRoute } from "../Calculations";
+import AboutUs , { route as aboutUsRoute } from "../AboutUs";
 import styled from "@emotion/styled";
 import SiteInfo from "../Home/SiteInfo";
 
@@ -23,7 +22,6 @@ const Content = () => {
         <Route path={homeRoute} element={<Home/>} />
         <Route path={productsRoute} element={<Products/>} />
         <Route path={aboutUsRoute} element={<AboutUs/>} />
-        <Route path={calculationsRoute} element={<Calculations/>} />
       </Routes>
     </RoutesContainer>
     <SiteInfo />
