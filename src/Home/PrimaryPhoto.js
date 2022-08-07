052e19c20e5080380d5acc93d267379fda4232f3
Alexander Hammonds <ahammonds247@gmail.com>
2022-08-07 15:22:52 -0500

    adding fall slides

--- src/Home/PrimaryPhoto.js
+++ src/Home/PrimaryPhoto.js
@@ -1,10 +1,12 @@
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 
-import firstImage from './slider-images/top/1-all-products.jpg'
-import secondImage from './slider-images/top/2-signature-candle.jpg'
-import thirdImage from './slider-images/top/3-more-products.jpg'
-import fourthImage from './slider-images/top/4-wax-melts.jpg'
+import firstImage from './slider-images/top/1-fall-scents.jpeg'
+import secondImage from './slider-images/top/2-fall-christmas-scents.jpeg'
+import thirdImage from './slider-images/top/3-all-products.jpg'
+import fourthImage from './slider-images/top/4-signature-candle.jpg'
+import fifthImage from './slider-images/top/5-more-products.jpg'
+import sixthImage from './slider-images/top/6-wax-melts.jpg'
 
 const carouselSettings = {
   showThumbs: false,
@@ -29,6 +31,12 @@ const PrimaryPhoto = () => {
       </div>
       <div>
         <img src={fourthImage}  alt=''/>
+      </div>
+      <div>
+        <img src={fifthImage}  alt=''/>
+      </div>
+      <div>
+        <img src={sixthImage}  alt=''/>
       </div>
     </Carousel>
   </>);
