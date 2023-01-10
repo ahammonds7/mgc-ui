80d79d19bd2553bf830534a643e92c2498d6d1e6
Alexander Hammonds <alexander.hammonds@rgare.com>
2023-01-10 16:55:16 -0600

    making ttl 15 mins

--- deploy/modules/cloudfront.tf
+++ deploy/modules/cloudfront.tf
@@ -36,7 +36,7 @@ resource "aws_cloudfront_distribution" "mgc-ui_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
+    default_ttl            = 900
     max_ttl                = 86400
   }
 
