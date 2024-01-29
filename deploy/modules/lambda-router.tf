1c6537d64a7a717d6ef8dcad81f4cfc9e6058517
Alexander Hammonds <Alexander.Hammonds@rgare.com>
2024-01-29 14:56:19 -0600

    back 1.0 for cf function

--- deploy/modules/lambda-router.tf
+++ deploy/modules/lambda-router.tf
@@ -1,6 +1,6 @@
 resource "aws_cloudfront_function" "lambda-router" {
   name    = "lambda-router-${var.DEPLOY_ENV}"
-  runtime = "cloudfront-js-2.0"
+  runtime = "cloudfront-js-1.0"
   code    = file("${path.module}/router.js")
   comment = "used to route other domains to default domain"
 }
--- deploy/modules/router.js
+++ deploy/modules/router.js
@@ -1,13 +1,13 @@
-const mainHost = 'magnoliagrovecollective.com'
+let mainHost = 'magnoliagrovecollective.com'
 
-async function handler(event) {
-  const request = event.request;
-  const host = request.headers.host.value;
-  const path = request.uri
+function handler(event) {
+  let request = event.request;
+  let host = request.headers.host.value;
+  let path = request.uri
 
   if (!host.includes(mainHost)) {
-    const subdomain = host.split('.')[0] === mainHost ? '' : `${host.split('.')[0]}.`
-    const response = {
+    let subdomain = host.split('.')[0] === mainHost ? '' : `${host.split('.')[0]}.`
+    let response = {
       statusCode: 301,
       statusDescription: 'Moved Permanently',
       headers:
